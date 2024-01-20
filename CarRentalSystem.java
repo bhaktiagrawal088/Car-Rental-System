1b5a7b13ea3f40855bb6a6ae0ca7a3f54cf83e49
bhaktiagrawal088 <bhaktiagrawal286@gmail.com>
2024-01-20 21:48:38 +0530

    rentalsystem

--- CarRentalSystem.java
+++ CarRentalSystem.java
@@ -73,30 +73,29 @@ public class CarRentalSystem {
                     }
                 }
                 System.out.println("Enter the car Id you want to rent: ");
-                String carId = sr.nextLine();
-                sr.nextLine();
-
-                System.out.println("Enter the number of days for rentals");
-                int rentalDays = sr.nextInt();
-
-                Customer newCustomer = new Customer("CUS" + (customers.size() + 1), customerName, phoneNumber);
-                addCustomer(newCustomer);
+                String CarId = sr.nextLine();
                 sr.nextLine();
 
                 Car selectedCar = null;
                 for (Car c : cars) {
-                    if (c.getcarID().equals(carId) && c.isAvaiable()) {
+                    if (c.getcarID().equals(CarId) && c.isAvaiable()) {
                         selectedCar = c;
                         break;
                     }
                 }
                 if (selectedCar != null) {
+                    System.out.println("Enter the number of days for rentals");
+                    int rentalDays = sr.nextInt();
+
+                    Customer newCustomer = new Customer("CUS" + (customers.size() + 1), customerName, phoneNumber);
+                    addCustomer(newCustomer);
+
                     double totalPrice = selectedCar.calculatePrice(rentalDays);
                     System.out.println("*****Rental Information *****\n");
                     System.out.println("Customer Id: " + newCustomer.getCustomerId());
                     System.out.println("Customer Name: " + newCustomer.getName());
-                    System.out.println("Customer Name: " + newCustomer.getPhoneNumber());
-                    System.out.println("Cars: " + toString());
+                    System.out.println("Customer Phonenumber: " + newCustomer.getPhoneNumber());
+                    System.out.println("Cars: " + selectedCar.toString());
                     System.out.println("Rental Days: " + rentalDays);
                     System.out.println("Total Price: $%.2f%n" + totalPrice);
 
